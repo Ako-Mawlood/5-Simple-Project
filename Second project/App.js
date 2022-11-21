7d0de548fdf85d8bb784681ec9bbb16e79f4e784
Ako Mawlood <ako.mawlood22@gmail.com>
2022-11-21 16:38:01 +0300

    Finished second project

--- Second project/App.js
+++ Second project/App.js
@@ -1,14 +1,17 @@
 const container=document.querySelector(".container");
+const content=document.querySelector(".content");
 const openBtn=document.querySelector(".open-btn");
 const closeBtn=document.querySelector(".close-btn");
 
 openBtn.addEventListener("click",showContent);
 function showContent(){
 container.style.display="block";
+
 }
  
 closeBtn.addEventListener("click",hideContent);
 function hideContent(){
-	container.style.display="none"
-	
+	container.style.display="none";
+content.style.transform="translateY()"
+
 }
